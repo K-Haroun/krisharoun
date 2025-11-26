8d8ee695340bcbee03fe6cf639edf4cd2b1ee5b8
Kris Haroun <kris.haroun@gmail.com>
2025-11-26 15:28:18 +0000

    update project section

--- src/pages/Welcome/WelcomeProjects.vue
+++ src/pages/Welcome/WelcomeProjects.vue
@@ -14,34 +14,20 @@ import Heading from "@/components/Heading.vue";
 
     <div class="mx-auto pt-10 pb-10 md:pb-20">
       <div class="flex justify-center flex-wrap mx-auto gap-5">
-          <ProjectCard
-            link="https://www.gp-logistics.co.uk/"
-            title="GP Logistics"
-            :img="gplogistics"
-            v-motion-slide-visible-once-bottom
-          >
-            Assigned by company director to work on company website. Drastically improved
-            website SEO, speed and functionality, modernised and redesigned navigation UI,
-            added multiple new pages and components.
-          </ProjectCard>
-          <ProjectCard
-            link="https://www.gpl-customs.co.uk/"
-            title="GPL Customs"
-            :img="gplcustoms"
-            v-motion-slide-visible-once-bottom
-          >
-            Same company, their second website.
-          </ProjectCard>
-          <ProjectCard
-            link="https://www.dnd.co.uk/"
-            title="DnD"
-            :img="dnd"
-            v-motion-slide-visible-once-bottom
-          >
-            A game web app I built using Laravel and Vue. Full SPA, built for personal use
-            for me and my friends for our DnD sessions.
-            <p class="text-red-500 mt-3">Website currently not live.</p>
-          </ProjectCard>
+        <ProjectCard link="https://www.gp-logistics.co.uk/" title="GP Logistics" :img="gplogistics"
+          v-motion-slide-visible-once-bottom>
+          Assigned by company director to work on company website. Drastically improved
+          website SEO, speed and functionality, modernised and redesigned navigation UI,
+          added multiple new pages and components.
+        </ProjectCard>
+        <ProjectCard link="https://www.gpl-customs.co.uk/" title="GPL Customs" :img="gplcustoms"
+          v-motion-slide-visible-once-bottom>
+          Same company, their second website.
+        </ProjectCard>
+        <ProjectCard link="https://fpllegends.on-forge.com/" title="FPL Legends" :img="fpllegends"
+          v-motion-slide-visible-once-bottom>
+          Fantasy Premier League web app solo built using Laravel / Inertia / Vue & official FPL API.
+        </ProjectCard>
       </div>
     </div>
   </div>
